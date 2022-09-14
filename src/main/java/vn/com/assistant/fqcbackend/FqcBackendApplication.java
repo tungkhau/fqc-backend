4dcbe3d7c1976a2b3ca2f405874c1cd76ef0301f
Tung Khau <55617424+jasonkhau@users.noreply.github.com>
2022-09-14 09:33:20 +0700

    Initial commit

---
+++ src/main/java/vn/com/assistant/fqcbackend/FqcBackendApplication.java
@@ -0,0 +1,13 @@
+package vn.com.assistant.fqcbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FqcBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FqcBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/com/assistant/fqcbackend/FqcBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.com.assistant.fqcbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FqcBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
