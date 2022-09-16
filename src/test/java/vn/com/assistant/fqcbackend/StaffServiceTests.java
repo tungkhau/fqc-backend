6b7afbc115c1fa98a342fa0e0c24072381f323dd
V.T.TuongDi <vothituongduy20@gmail.com>
2022-09-16 14:21:41 +0700

    complete test staff service

--- src/test/java/vn/com/assistant/fqcbackend/StaffServiceTests.java
+++ src/test/java/vn/com/assistant/fqcbackend/StaffServiceTests.java
@@ -5,17 +5,22 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ContextConfiguration;
 import vn.com.assistant.fqcbackend.dto.StaffRequestDTO;
 import vn.com.assistant.fqcbackend.entity.Role;
 import vn.com.assistant.fqcbackend.entity.User;
+import vn.com.assistant.fqcbackend.exception.InvalidException;
 import vn.com.assistant.fqcbackend.repository.UserRepository;
 import vn.com.assistant.fqcbackend.service.StaffServiceImp;
 import vn.com.assistant.fqcbackend.utility.StaffMapper;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -28,6 +33,8 @@ import static org.mockito.Mockito.*;
 public class StaffServiceTests {
     @Mock
     UserRepository userRepository;
+    @Mock
+    PasswordEncoder passwordEncoder;
     @Mock
     Environment env;
     @Captor
@@ -42,8 +49,6 @@ public class StaffServiceTests {
         userArgumentCaptor = ArgumentCaptor.forClass(User.class);
 
         User user = StaffMapper.INSTANCE.staffRequestDTOtoUser(requestDTO);
-
-        given(userRepository.findByCode(requestDTO.getCode())).willReturn(null);
         //when
         staffService.create(requestDTO);
         //then
@@ -52,27 +57,51 @@ public class StaffServiceTests {
 
         assertThat(capturedCustomer)
                 .usingRecursiveComparison()
-                .ignoringFields("id", "")
+                .ignoringFields("id", "encryptedPassword")
                 .isEqualTo(user);
     }
 
     @Test
-    void createFailedCodeDuplicated(){
-        //given
+    void canUpdate(){
+        //give
         StaffRequestDTO requestDTO = genMockStaffRequest();
-        User user = StaffMapper.INSTANCE.staffRequestDTOtoUser(requestDTO);
-        given(userRepository.findByCode(requestDTO.getCode())).willReturn(user);
+        String staffId = UUID.randomUUID().toString();
+        User user = genMockStaff();
+        user.setId(staffId);
+        given(userRepository.findById(user.getId())).willReturn(Optional.of(user));
+        userArgumentCaptor = ArgumentCaptor.forClass(User.class);
 
-        when(env.getProperty("staff.code.existed")).thenReturn("Msg");
+        //when
+        staffService.update(requestDTO, staffId);
 
         //then
-        Assertions.assertThatThrownBy(()-> staffService.create(requestDTO))
-                .hasMessageContaining("Msg");
-
-        verify(userRepository, never()).save(any());
-
+        Mockito.verify(userRepository).save(userArgumentCaptor.capture());
+        User capturedCustomer = userArgumentCaptor.getValue();
+        assertThat(capturedCustomer)
+                .usingRecursiveComparison()
+                .ignoringFields("encryptedPassword")
+                .isEqualTo(user);
     }
 
+    @Test
+    void updateFailedNotFound(){
+        //given
+        StaffRequestDTO requestDTO = genMockStaffRequest();
+        String staffId = UUID.randomUUID().toString();
+        given(userRepository.findById(staffId)).willReturn(Optional.empty());
+        when(env.getProperty("staff.notExisted")).thenReturn("Msg");
+
+        //when & then
+        Assertions.assertThatThrownBy(()-> staffService.update(requestDTO, staffId))
+                .isInstanceOf(InvalidException.class)
+                .hasMessageContaining("Msg");
+        verify(userRepository, never()).save(any());
+    }
+
+    @Test
+    void canResetPassword() {
+
+    }
 
     private StaffRequestDTO genMockStaffRequest(){
         StaffRequestDTO requestDTO = new StaffRequestDTO();
@@ -80,6 +109,10 @@ public class StaffServiceTests {
         requestDTO.setName("Nguyen Van A");
         requestDTO.setRole(Role.STAFF);
         return  requestDTO;
+    }
+
+    private User genMockStaff(){
+        return new User(UUID.randomUUID().toString(),"STAFF", "Nguyen Van B", "12345", "STAFF", false);
     }
 
 }
