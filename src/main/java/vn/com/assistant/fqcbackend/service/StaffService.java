740a492e025466089fa686bc033a73a7d5a763b8
V.T.TuongDi <vothituongduy20@gmail.com>
2022-09-16 10:58:51 +0700

    test staff service

--- src/main/java/vn/com/assistant/fqcbackend/service/StaffService.java
+++ src/main/java/vn/com/assistant/fqcbackend/service/StaffService.java
@@ -1,6 +1,5 @@
 package vn.com.assistant.fqcbackend.service;
 
-import org.springframework.stereotype.Service;
 import vn.com.assistant.fqcbackend.dto.StaffRequestDTO;
 
 public interface StaffService {
